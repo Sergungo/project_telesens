6e313031c7272d115e58953c7f3bff6ef1e7bf27
Sergey Golenko <serjgolenko@gmail.com>
2019-01-17 22:59:41 +0200

    classes added

---
+++ src/ua/golenko/simulation/Model/Bus.java
@@ -0,0 +1,9 @@
+package ua.golenko.simulation.Model;
+
+public class Bus {
+    private final int capacity = 60;
+    private double speed;
+    private BusStop routStart;
+    private BusStop routFinish;
+
+}
---
+++ src/ua/golenko/simulation/Model/BusStop.java
@@ -0,0 +1,4 @@
+package ua.golenko.simulation.Model;
+
+public enum BusStop {
+}
